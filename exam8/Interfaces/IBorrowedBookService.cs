c3ea943f1727d0dd52a57d691623c06fdcb8c64a
Alexandr Makedonsky <jaxylyk.rahimov@gmail.com>
2025-03-08 15:35:51 +0500

    IBorrowedBookService.cs

---
+++ exam8/Interfaces/IBorrowedBookService.cs
@@ -0,0 +1,12 @@
+﻿using exam8.Models;
+
+namespace exam8.Interfaces;
+
+public interface IBorrowedBookService
+{
+    Task<bool> BorrowBookAsync(int id, string userEmail);
+    Task<bool> ReturnBookAsync(int id, string userEmail);
+    Task<IEnumerable<BorrowedBook>> GetBorrowedBooksAsync();
+    Task<IEnumerable<BorrowedBook>> GetBooksAsync(string userEmail);
+    
+}
