0e7e99ee3f2c4e4feee3a396021f300598f67476
Alexandr Makedonsky <jaxylyk.rahimov@gmail.com>
2025-03-08 15:25:38 +0500

    IBookService.cs

---
+++ exam8/Interfaces/IBookService.cs
@@ -0,0 +1,12 @@
+﻿using exam8.Models;
+
+namespace exam8.Interfaces;
+
+public interface IBookService
+{
+    Task<int> AddBookAsync(Book book);
+    Task<IEnumerable<Book>> GetBooksAsync(int page, int pageSize);
+    Task<Book?> GetBookByIdAsync(int id);
+    Task<bool> UpdateBookAsync(int id, Book book);
+    Task<bool> DeleteBookAsync(int id);
+}
---
+++ exam8/Services/BookService.cs
@@ -0,0 +1,32 @@
+﻿using exam8.Interfaces;
+using exam8.Models;
+
+namespace exam8.Services;
+
+public class BookService(IBookRepository bookRepository) : IBookService
+{
+    public Task<int> AddBookAsync(Book book)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Book>> GetBooksAsync(int page, int pageSize)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Book?> GetBookByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateBookAsync(int id, Book book)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteBookAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
