98913f545b765d4a2688b7cbcd65f6f82a02f135
Alexandr Makedonsky <jaxylyk.rahimov@gmail.com>
2025-03-08 15:02:03 +0500

    repo

---
+++ exam8/Interfaces/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using exam8.Models;
+
+namespace exam8.Interfaces;
+
+public interface IUserRepository
+{
+    Task<int> AddUserAsync(User user);
+    Task<User> GetUserByEmailAsync(string Email);
+}
---
+++ exam8/Repositories/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+using Dapper;
+using exam8.Interfaces;
+using exam8.Models;
+
+namespace exam8.Repositories;
+
+public class UserRepository(IDbConnection connection) : IUserRepository
+{
+    public async Task<int> AddUserAsync(User user)
+    {
+        var sql = """
+                  INSERT INTO users
+                      (first_name, last_name, email, phone_number)
+                  VALUES
+                      (@first_name, @last_name, @email, @phone_number)
+                  RETURNING id;
+                  """;
+        return await connection.QuerySingleAsync<int>(sql, user);
+    }
+
+    public async Task<User> GetUserByEmailAsync(string Email)
+    {
+        var sql = "SELECT * FROM users WHERE email = @email";
+        return await connection.QuerySingleAsync<User>(sql, new { email = Email });
+    }
+}
