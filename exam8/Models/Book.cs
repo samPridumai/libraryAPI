5da02236307d9151d25723f9b79fbf16e416477b
Alexandr Makedonsky <jaxylyk.rahimov@gmail.com>
2025-03-08 12:45:25 +0500

    model book

---
+++ exam8/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace exam8.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = null!;
+    public string Author { get; set; } = null!;
+    public string CoverImageUrl { get; set; } = null!;
+    public int PublicationYear { get; set; }
+    public string Description { get; set; } = null!;
+    public string Status { get; set; } = "available";
+    public DateTime CreatedAt { get; set; }
+}
