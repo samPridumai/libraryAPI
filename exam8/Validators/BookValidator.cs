3be3b97a4b821be56739b7f386341fe184027d12
Alexandr Makedonsky <jaxylyk.rahimov@gmail.com>
2025-03-08 18:57:28 +0500

    BookValidator.cs

---
+++ exam8/Validators/BookValidator.cs
@@ -0,0 +1,18 @@
+﻿using exam8.Models;
+using FluentValidation;
+
+namespace exam8.Validators;
+
+public class BookValidator : AbstractValidator<Book>
+{
+    public BookValidator()
+    {
+        RuleFor(b => b.Title)
+            .NotEmpty().WithMessage("название книги обязательно")
+            .MaximumLength(255).WithMessage("не больше 255");
+        
+        RuleFor(b => b.Author)
+            .NotEmpty().WithMessage("автор книги обязательно")
+            .MaximumLength(255).WithMessage("не больше 255");
+    }
+}
