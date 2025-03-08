8fd4900e94b090548958fcc53adea0927f64063c
Alexandr Makedonsky <jaxylyk.rahimov@gmail.com>
2025-03-08 15:52:34 +0500

    BooksController.cs через сервис

--- exam8/Controllers/BooksController.cs
+++ exam8/Controllers/BooksController.cs
@@ -6,50 +6,32 @@ namespace exam8.Controllers;
 
 [ApiController]
 [Route("api/books")]
-public class BooksController(IBookRepository bookRepository) : ControllerBase
+public class BooksController(IBookService bookService) : ControllerBase
 {
     [HttpPost]
     public async Task<ActionResult<Book>> Create([FromBody] Book book)
     {
-        if (book == null)
+        var bookId = await bookService.AddBookAsync(book);
+        if (bookId == 0)
         {
             return BadRequest(new { message = "неверные данные" });
         }
-
-        var bookId = await bookRepository.AddBookAsync(book);
-        var createdBook = await bookRepository.GetBookByIdAsync(bookId);
         
-        return CreatedAtAction(nameof(GetBookById), new { id = bookId }, createdBook);
+        var createdBook = await bookService.GetBookByIdAsync(bookId);
+        return CreatedAtAction(nameof(GetBookById), new {id = bookId}, createdBook);
     }
 
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Book>>> GetBooks([FromQuery] int page = 1, [FromQuery] int pageSize = 8)
     {
-        if (page < 1 || pageSize < 1)
-        {
-            return BadRequest(new { message = "неверные данные" });
-        }
-
-        var books = await bookRepository.GetBooksAsync(page, pageSize);
-        var totalCount = await bookRepository.GetTotalBooksCountAsync(); 
-
-        if (!books.Any())
-        {
-            return NotFound(new { message = "не найдено" });
-        }
-
-        return Ok(new
-        {
-            page,
-            totalPages = (int)Math.Ceiling((double)totalCount / pageSize),
-            books
-        });
+        var books = await bookService.GetBooksAsync(page, pageSize);
+        return Ok(books);
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Book>> GetBookById(int id)
     {
-        var book = await bookRepository.GetBookByIdAsync(id);
+        var book = await bookService.GetBookByIdAsync(id);
         if (book == null)
         {
             return NotFound(new { message = "не найдено" });
@@ -60,40 +42,20 @@ public class BooksController(IBookRepository bookRepository) : ControllerBase
     [HttpPut("{id}")]
     public async Task<IActionResult> UpdateBook(int id, [FromBody] Book book)
     {
-        if (book == null)
-        {
-            return BadRequest(new { message = "неверные данные" });
-        }
-
-        var exists = await bookRepository.GetBookByIdAsync(id);
-        if (exists == null)
+        var updatedBook = await bookService.UpdateBookAsync(id, book);
+        if (!updatedBook)
         {
             return NotFound(new { message = "не найдено" });
         }
-
-        var updated = await bookRepository.UpdateBookAsync(id, book);
-        if (!updated)
-        {
-            return BadRequest(new { message = "неверные данные" });
-        }
-
-        return Ok(new { message = "Книга успешно обновлена" }); 
+        return Ok(new { message = "Книга успешно обновлена" });
     }
 
     [HttpDelete("{id}")]
     public async Task<IActionResult> DeleteBook(int id)
     {
-        var exists = await bookRepository.GetBookByIdAsync(id);
-        if (exists == null)
-        {
+        var deletedBook = await bookService.DeleteBookAsync(id);
+        if (!deletedBook)
             return NotFound(new { message = "не найдено" });
-        }
-
-        var deleted = await bookRepository.DeleteBookAsync(id);
-        if (!deleted)
-        {
-            return BadRequest(new { message = "неверные данные" });
-        }
 
         return Ok(new { message = "Книга успешно удалена" });
     }
