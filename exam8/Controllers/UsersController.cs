65acbe880c1e821290dccd302d9eb8bfa76e9074
Alexandr Makedonsky <jaxylyk.rahimov@gmail.com>
2025-03-08 16:01:59 +0500

    UsersController.cs

---
+++ exam8/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using exam8.Interfaces;
+using exam8.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace exam8.Controllers;
+
+[ApiController]
+[Route("/api/users/")]
+public class UsersController(IUserService userService) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> RegisterUser([FromBody] User user)
+    {
+        var userId = await userService.RegisterUserAsync(user);
+        if (userId == 0)
+            return BadRequest(new { message = "ошибка при регистрации" });
+        
+        var createdUser = await userService.GetUserByEmailAsync(user.Email);
+        return CreatedAtAction(nameof(GetUserByEmail), new { email = user.Email}, createdUser);
+    }
+
+    [HttpGet("{email}")]
+    public async Task<IActionResult> GetUserByEmail(string email)
+    {
+        var user = await userService.GetUserByEmailAsync(email);
+        if (user == null)
+            return NotFound(new { message = "пользователь не найден" });
+
+        return Ok(user);
+    }
+}
