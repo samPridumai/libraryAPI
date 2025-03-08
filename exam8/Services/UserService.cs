dd5302fa524322b741e3f5bc9b083f5cb1452fe0
Alexandr Makedonsky <jaxylyk.rahimov@gmail.com>
2025-03-08 15:33:09 +0500

    UserService.cs

---
+++ exam8/Services/UserService.cs
@@ -0,0 +1,17 @@
+﻿using exam8.Interfaces;
+using exam8.Models;
+
+namespace exam8.Services;
+
+public class UserService(IUserRepository userRepository) : IUserService
+{
+    public async Task<int> RegisterUserAsync(User user)
+    {
+        return await userRepository.AddUserAsync(user);
+    }
+
+    public async Task<User?> GetUserByEmailAsync(string email)
+    {
+        return await userRepository.GetUserByEmailAsync(email);
+    }
+}
