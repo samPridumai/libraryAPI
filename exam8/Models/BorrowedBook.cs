c4879297ee17fa419caa9d8e8add5dc9192c58db
Alexandr Makedonsky <jaxylyk.rahimov@gmail.com>
2025-03-08 14:51:18 +0500

    models

---
+++ exam8/Models/BorrowedBook.cs
@@ -0,0 +1,9 @@
+﻿namespace exam8.Models;
+
+public class BorrowedBook
+{
+    public int Id { get; set; }
+    public int BookId { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public DateTime BorrowedAt { get; set; } = DateTime.UtcNow;
+}
